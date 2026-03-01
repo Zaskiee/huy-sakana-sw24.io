09da3197db0c1d0a4d2bb720f2a40d0913dc8365
Zaskiee <sakanahuy168@gmail.com>
2026-03-02 02:04:18 +0700

    update code number wont repeat

--- js/random.js
+++ js/random.js
@@ -1,15 +1,22 @@
 var intervalId = null;
 var isRunning=false;
+let random;
+let all = [10, 20, 30, 40, 50];
+var highest_num = 100
 
 function generateNumber(){
     if(!isRunning) {
         //startloop
         intervalId=setInterval(() => {
             for (let i = 1; i<=5; i++){
-            var random = Math.floor(Math.random()*100) + 1;
-            document.getElementById("n"+i).innerHTML = random; 
+                random = Math.floor(Math.random()*highest_num) + 1;
+                while (all.includes(random)){
+                    random = Math.floor(Math.random()*highest_num) + 1;
+                }
+                document.getElementById("n"+i).innerHTML = random; 
+                all[i-1] = random;
             }
-        }, 80);
+        }, 3);
         document.getElementById("btn").innerHTML="stop";
         isRunning = true
     } else {
